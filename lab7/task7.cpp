6213bdd2a584bf13d0e56dedbb3b1c92c8e0955e
DavidBao03 <1353406961@qq.com>
2023-10-24 11:42:16 +0000

    new lab7

---
+++ lab7/task7.cpp
@@ -0,0 +1,108 @@
+#include <bits/types/struct_timeval.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+struct matrix {
+	double* data;
+	int rows;
+	int cols;
+};
+
+struct matrix* matrix_create(int rows, int cols) {
+	struct matrix* m = (struct matrix *)malloc(sizeof(struct matrix));
+	m -> rows = rows;
+	m -> cols = cols;
+	m -> data = (double *)malloc(sizeof(double) * rows * cols);
+	return m;
+}
+
+void matrix_destroy(struct matrix* m) {
+	free(m -> data);
+	free(m);
+}
+
+void matrix_random_init(struct matrix* m) {
+	for (int i = 0; i < m -> rows * m -> cols; i++) {
+		m -> data[i] = (double)rand() / RAND_MAX;
+	}
+}
+
+void random_init() {
+	srand(time(NULL));
+}
+
+int check(struct matrix* a, struct matrix* b) {
+	if (a -> rows != b -> rows || a -> cols != b -> cols) {
+		return 0;
+	}
+	for (int i = 0; i < a -> rows * a -> cols; i++) {
+		if (a -> data[i] != b -> data[i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+struct matrix* matrix_multiple(struct matrix* a, struct matrix* b) {
+	struct matrix* c = matrix_create(a -> rows, b -> cols);
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	for (int i = 0; i < a -> rows; i++) {
+		for (int j = 0; j < b -> cols; j++) {
+		double sum = 0;
+		for (int k = 0; k < a -> cols; k++) {
+			sum += a -> data[i * a -> cols + k] * b -> data[k * b -> cols + j];
+		}
+		c -> data[i * c -> cols + j] = sum;
+		}
+	}
+
+	gettimeofday(&end, NULL);
+	printf("before optimize: %ld\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+	return c;
+}
+
+struct matrix* matrix_multiple_optimize(struct matrix* a, struct matrix* b) {
+	struct matrix* c = matrix_create(a -> rows, b -> cols);
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	//-----------------------------------
+	for (int i = 0; i < a -> rows; i++) {
+		for(int j = 0; j < b -> cols; j++){
+			double sum = 0;
+			for(int k = 0; k < a-> cols; k++){
+				sum += a -> data[i * a -> cols + k] *
+					b -> data[k * b -> cols + j];
+			}
+			c -> data[i * c -> cols + j] = sum;
+		}
+	}
+	//------------------------------------
+	gettimeofday(&end, NULL);
+	printf("after optimize: %ld\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+	return c;
+}
+
+int main() {
+	random_init();
+	struct matrix* m = matrix_create(1000, 2000);
+	matrix_random_init(m);
+	struct matrix* n = matrix_create(2000, 3000);
+	matrix_random_init(n);
+	struct matrix* r = matrix_multiple(m, n);
+	struct matrix* r_optimize = matrix_multiple_optimize(m, n);
+	if (check(r, r_optimize)) {
+		printf("check ok\n");
+	} else {
+		printf("check failed\n");
+	}
+	
+	matrix_destroy(m);
+	matrix_destroy(n);
+	
+	matrix_destroy(r);
+	matrix_destroy(r_optimize);
+	return 0;
+}
